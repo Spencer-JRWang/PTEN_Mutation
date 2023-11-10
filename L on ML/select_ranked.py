6cca22554dae457b6d8fcc23a7224925128c42d0
LoveUCB <140378072+LoveUCB@users.noreply.github.com>
2023-11-10 08:26:54 +0800

    Add files via upload

--- L on ML/select_ranked.py
+++ L on ML/select_ranked.py
@@ -5,14 +5,15 @@ def select(file, number):
     for i in list_raw:
         list_1 = []
         s = i.strip('\n').split('\t')
-        for j in s:
+        for j in (s[0], s[2]):
             m = eval(j)
             list_1.append(m)
+        list_1.append(s[1])
         list_dat.append(list_1)
-    sorted_list = sorted(list_dat, key=lambda x: x[2], reverse=True)
+    sorted_list = sorted(list_dat, key=lambda x: x[1], reverse=True)
     sorted_list = sorted_list[:number]
     for n in range(len(sorted_list)):
         print("排名" + str(n + 1) + "的模型是: ", end = '')
         print(sorted_list[n])
 
-select("ASD versus ASD_Cancer.txt", 10)
+select("Cancer versus ASD_Cancer.txt", 5)
